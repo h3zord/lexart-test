5a8f3c950dbe3e25dbdf9bf699b1c65c9b83aae1
Lucas <lucaschmain@gmail.com>
2024-03-07 17:53:35 -0300

    script: config scripts to deploy

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,17 +1,8 @@
 import 'dotenv/config'
 import { app } from './app'
 import { testConnectionToDB } from './database/config/database'
-import { createJwtToken } from './utils/auth'
 
 const port = process.env.PORT || '3000'
-
-const token = createJwtToken({
-  fullName: 'Lucas Chavarem',
-  email: 'test@test.com',
-  accountType: 'admin',
-})
-
-console.log(token)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
